51522787d70e193bd091a45028fe914cf13392a2
Nati <natirosh@gmail.com>
2018-12-18 12:27:35 +0200

    folder 5: keywords and expressions_1

--- src/com/natirosh/KeywordsAndExpressions.java
+++
@@ -1,9 +0,0 @@
-package com.natirosh;
-
-public class KeywordsAndExpressions {
-
-    public static void main(String[] args) {
-        System.out.println("Hello1234");
-	// write your code here NATI
-    }
-}
