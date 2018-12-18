f77d8ff6d571c0fc93e904b6e5e6fe36f131c443
Nati <natirosh@gmail.com>
2018-12-18 11:37:56 +0200

    End of folder 4: operators

--- src/com/natirosh/Operators.java
+++ src/com/natirosh/Operators.java
@@ -15,5 +15,28 @@ public class Operators {
         if ((topScore > 90) || (secondTopScore <= 90))
             System.out.println("One of these tests is true");
 
-    }
+        int newValue = 50;
+        if(newValue ==50)
+            System.out.println("This is true");
+        boolean isCar = false;
+        if(isCar == true)
+            System.out.println("This is not supposed to happened");
+
+/*challenge:
+ * 1. create a double veriable with the value of 20
+ * 2. create additional with value ot 80
+ * 3. add both numbers and multiply by 25
+ * 4. check what is the reminder from last calc divided by 40
+ * 5. if the reminder is less or equal to 40 write a message 'total is over the limit'
+ */
+        double verDouble1 = 20d;
+        double verDouble2 = 80d;
+        double summary = (verDouble1 + verDouble2)* 25;
+        double reminder = summary % 40;
+        System.out.println("reminder is: " + reminder );
+        if(reminder <= 20)
+            System.out.println("Total is over the limit");
+        }
+
+
 }
