6207782ce802e2f05b29d6e08a49261ed638ddb4
nati <natirosh@gmail.com>
2018-12-19 09:04:46 +0200

    Lesson 5.3 statements

---
+++ src/com/natirosh/Statements.java
@@ -0,0 +1,8 @@
+package com.natirosh;
+
+public class Statements {
+    public static void main(String[] args) {
+        System.out.println("Statements");
+        // write your code here NATI
+    }
+}
