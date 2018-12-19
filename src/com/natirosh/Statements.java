4e477ab99290876a0dc8df19dec825acb946ad38
nati <natirosh@gmail.com>
2018-12-19 09:35:01 +0200

    Lesson 5.3 statements_1

--- src/com/natirosh/Statements.java
+++ src/com/natirosh/Statements.java
@@ -3,6 +3,16 @@ package com.natirosh;
 public class Statements {
     public static void main(String[] args) {
         System.out.println("Statements_1");
-        // write your code here NATI
+        int myVariable = 50;
+        myVariable++;
+        System.out.println(myVariable);
+        myVariable--;
+        System.out.println(myVariable);
+        System.out.println("This is a test");
+
+        System.out.println("This is " +
+                "one more " +
+                "line");
+
     }
 }
