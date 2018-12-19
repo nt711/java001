90726a7df2f83f0cc27da108537d32ff1cb0f1ed
Nati <natirosh@gmail.com>
2018-12-19 13:35:55 +0200

    Remove: keywords and expressions

--- src/com/natirosh/KeywordsAndExpressions.java
+++
@@ -1,8 +0,0 @@
-package com.natirosh;
-
-public class KeywordsAndExpressions {
-    public static void main(String[] args) {
-        System.out.println("KeywordsAndExpressions");
-        // write your code here NATI
-    }
-}
