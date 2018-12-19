3829ba245e5b06b9be9c065688fcc39f1950860f
nati <natirosh@gmail.com>
2018-12-19 09:42:02 +0200

    Lesson 5.3 statements_3

--- src/com/natirosh/Statements.java
+++ src/com/natirosh/Statements.java
@@ -2,7 +2,7 @@ package com.natirosh;
 
 public class Statements {
     public static void main(String[] args) {
-        System.out.println("Statements_2");
+        System.out.println("Statements_3");
         int myVariable = 50;
         myVariable++;
         System.out.println(myVariable);
