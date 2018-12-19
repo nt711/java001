85ace698bc3218d4f63f0344ca98a37420e94b40
Nati <natirosh@gmail.com>
2018-12-19 17:30:04 +0200

    if Keywords_1

---
+++ src/com/natirosh/ifKeyword.java
@@ -0,0 +1,30 @@
+package com.natirosh;
+
+public class ifKeyword {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+//        if (score < 5000 && score > 1000) {
+//            System.out.println("Your score was less then 5000 but more then 1000");
+//        } else  if (score < 1000) {
+//            System.out.println("Score is less then 1000");
+//        } else {
+//            System.out.println("Got here");
+        if (gameOver == true) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        if (gameOver == true) {
+            int finalScore;
+            finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+}
