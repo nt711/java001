7f6e63cdddaab045aeda3c7b194f64d3ae9ddf3d
nati <natirosh@gmail.com>
2018-12-28 16:50:13 +0200

    Methods 02 challenge

---
+++ src/com/natirosh/a5_Methods_2_chalange.java
@@ -0,0 +1,56 @@
+package com.natirosh;
+
+public class a5_Methods_2_chalange {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+
+        displayHighScorePosition("Player1", calculateHighScorePosition(600));
+
+        displayHighScorePosition("Player2", calculateHighScorePosition(300));
+
+        displayHighScorePosition("Player3", calculateHighScorePosition(40));
+
+        displayHighScorePosition("Player4", calculateHighScorePosition(5000));
+    }
+
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + " managed to get into position " + position + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore > 1000) {
+            return 1;
+        } else if (playerScore > 500 && playerScore < 1000) {
+            return 2;
+        } else if (playerScore > 100 && playerScore < 500) {
+            return 3;
+        } else return 4;
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int leverComplete, int bonus) {
+        if (gameOver) {
+            int finalScore = score + (leverComplete * bonus);
+            finalScore += 2000;
+            return finalScore;
+
+        } else {
+            return -1;
+        }
+    }
+
+}
