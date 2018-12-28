a3cf522d005c754ebbde4b675052799657e8a11d
nati <natirosh@gmail.com>
2018-12-28 02:05:08 +0200

    Methods 02

--- src/com/natirosh/a5_Methods.java
+++ src/com/natirosh/a5_Methods.java
@@ -4,12 +4,12 @@ public class a5_Methods {
     public static void main(String[] args) {
         calculateScore(true,800,5,100);
         calculateScore(true,10000,8,200);
-
     }
 
     public static void calculateScore(boolean gameOver,int score,int leverComplete,int bonus) {
         if (gameOver) {
             int finalScore = score + (leverComplete * bonus);
+            finalScore +=2000;
             System.out.println("Your final score was " + finalScore);
         }
 
