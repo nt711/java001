b18a41785b3f69f2e9bbdf02193a611e49366519
Nati <natirosh@gmail.com>
2019-01-03 17:40:58 +0200

    a5_Methods_7_Seconds_Minutes_challenge

---
+++ src/com/natirosh/a5_Methods_7_Seconds_Minutes.java
@@ -0,0 +1,17 @@
+package com.natirosh;
+
+public class a5_Methods_7_Seconds_Minutes {
+    public static void main(String[] args) {
+
+    }
+    public static String getDurationString (int minutes, int seconds) {
+        if ((minutes <0) || (seconds <0 || seconds >60)) {
+            return "Invalid Value";
+        }
+        if (minutes >=60) {
+            int hours = minutes / 60;
+//            while minutes >= ???
+        }
+        return "aa"
+    }
+}
