7afec8ce372044ae667335d8288a05359b91bb83
nati <natirosh@gmail.com>
2019-01-05 22:22:39 +0200

    Sec+Min challenge_1

---
+++ src/com/natirosh/a5_Methods_4_OverLoading.java
@@ -0,0 +1,17 @@
+package com.natirosh;
+
+public class a5_Methods_4_OverLoading {
+    public static void main(String[] args) {
+        double a = calcFeetAndInchesToCentimeters(6, 4);
+        System.out.println("Value is " + a + "cm");
+
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if (!(feet >= 0 || (inches >= 0 && inches <= 12))) {
+            return -1;
+        }
+        return (feet * 12) + (inches * 2.54);
+
+    }
+}
--- src/com/natirosh/a5_Methods_7_Seconds_Minutes.java
+++ src/com/natirosh/a5_Methods_7_Seconds_Minutes.java
@@ -2,18 +2,27 @@ package com.natirosh;
 
 public class a5_Methods_7_Seconds_Minutes {
     public static void main(String[] args) {
-        System.out.println(getDurationString(301, 22));
-
+        System.out.println(getDurationString(3709));
     }
-    public static String getDurationString (int minutes, int seconds) {
-        if ((minutes <0) || (seconds <0 || seconds >60)) {
+
+    public static String getDurationString(int minutes, int seconds) {
+        if ((minutes < 0) || (seconds < 0 || seconds > 60)) {
             return "Invalid Value";
         }
-        if (minutes >=60) {
-//            minutes += hours % 60;
-//            hours = minutes / 60;
+        int hours = minutes / 60;
+        minutes = minutes % 60;
 
-        }
         return hours + "h " + minutes + "m " + seconds + "s";
     }
+
+    public static String getDurationString(int seconds) {
+        if (seconds < 0) {
+            return "Invalid Value";
+        }
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+
+        return getDurationString(minutes,seconds);
+    }
+
 }
