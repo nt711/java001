7e2ae6ca9311bbb946b048d9f7e09cdc398d0813
Nati <natirosh@gmail.com>
2019-01-06 09:11:31 +0200

    a5_Methods_7_Seconds_Minutes_challenge

---
+++ src/com/natirosh/a5_Methods_7_Seconds_Minutes_BONUS.java
@@ -0,0 +1,46 @@
+package com.natirosh;
+
+public class a5_Methods_7_Seconds_Minutes_BONUS {
+    public static void main(String[] args) {
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-41));
+        System.out.println(getDurationString(65, 9));
+    }
+
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
+
+    private static String getDurationString(int minutes, int seconds) {
+        if ((minutes < 0) || (seconds < 0 || seconds > 59)) {
+            return INVALID_VALUE_MESSAGE;
+        }
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+
+        String hoursString = hours + "h";
+        if (hours< 10) {
+            hoursString = "0" + hoursString;
+        }
+        String minutesString = remainingMinutes + "m";
+        if (remainingMinutes< 10) {
+            minutesString = "0" + minutesString;
+        }
+        String secondsString = seconds + "s";
+        if (seconds< 10) {
+            secondsString = "0" + secondsString;
+        }
+
+        return hoursString + " "  + minutesString + " " + secondsString;
+    }
+
+    private static String getDurationString(int seconds) {
+        if (seconds < 0) {
+            return INVALID_VALUE_MESSAGE;
+        }
+        int minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
+
+        return getDurationString(minutes,remainingSeconds);
+    }
+
+}
