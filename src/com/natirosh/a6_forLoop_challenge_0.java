2440746d623530ec89dbf27d0bd7a93085ae322a
Nati <natirosh@gmail.com>
2019-01-06 18:42:41 +0200

    a6_forLoop_challenge

--- src/com/natirosh/a6_forLoop_challenge_0.java
+++ src/com/natirosh/a6_forLoop_challenge_0.java
@@ -2,7 +2,7 @@ package com.natirosh;
 
 public class a6_forLoop_challenge_0 {
     public static void main(String[] args) {
-        for (int i=2; i<9; i++) {
+        for (int i=8; i>1; i--) {
             System.out.println("10000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
 
         }
