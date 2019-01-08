987f257a7800ed71c1833fddd702dcaf2d3e667e
nati <natirosh@gmail.com>
2019-01-08 22:11:50 +0200

    a6 digits sum challenge

--- src/com/natirosh/a6_digit_sum_challenge.java
+++ src/com/natirosh/a6_digit_sum_challenge.java
@@ -2,20 +2,22 @@ package com.natirosh;
 
 public class a6_digit_sum_challenge {
     public static void main(String[] args) {
-        System.out.println(sumDigits(71));
+        System.out.println(sumDigits(1234));
+        System.out.println(sumDigits(-1234));
+        System.out.println(sumDigits(1));
+        System.out.println(sumDigits(123456789));
+
     }
     private static int sumDigits (int number){
         if (number <10) {
             return -1;
         }
         int sum = 0;
-        int lastDigit = 0;
-        while (number >=10) {
-            lastDigit = number % 10;
-            number = number / 10;
-            sum += lastDigit;
+        while (number >0) {
+            int digit = number % 10;
+            number /= 10;
+            sum += digit;
         }
-
-        return sum + number;
+        return sum;
     }
 }
